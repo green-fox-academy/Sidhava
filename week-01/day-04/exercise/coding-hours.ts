1ed4b732d606629da78a5a10883c7367571e331b
Dániel Sümegi <sumegidaniel@gmail.com>
2019-06-27 12:34:26 +0200

    took out the comments from coding-hours.ts

--- week-01/day-04/exercise/coding-hours.ts
+++ week-01/day-04/exercise/coding-hours.ts
@@ -1,5 +1,3 @@
-// An average Green Fox attendee codes 6 hours daily
-// The semester is 17 weeks long
 let semesterInWeeks: number = 17;
 let attendeeCodesDaily: number = 6;
 let weekDays = 5;
@@ -10,9 +8,7 @@ let workHoursWeekly: number = 52;
 let workHoursInSemester: number = workHoursWeekly * semesterInWeeks;
 let percentageOfWorkingHours: number = codeSpentInSemester / (workHoursInSemester / 100);
 
-// Print how many hours is spent with coding in a semester by an attendee,
-// if the attendee only codes on workdays.
+
 console.log(codeSpentInSemester);
-// Print the percentage of the coding hours in the semester if the average
-// work hours weekly is 52
+
 console.log(percentageOfWorkingHours);
