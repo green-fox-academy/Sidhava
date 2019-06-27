8a012aff6aa514ae468d334a6af69f4bf71d4bba
Dániel Sümegi <sumegidaniel@gmail.com>
2019-06-27 14:25:37 +0200

    define-basic.info.ts

---
+++ week-01/day-04/exercise/define-basic-info.ts
@@ -0,0 +1,6 @@
+let myNameIs: string = 'Dániel Sümegi';
+let myAgeIs: number = 25;
+let myHeightIs: number = 178;
+let marriedOrNot: boolean = false;
+
+console.log('My name is ' + myNameIs + ', my age is ' + myAgeIs + ', I am ' + myHeightIs + ' centimeters tall. I am married.. that is ' + marriedOrNot +  '.');
