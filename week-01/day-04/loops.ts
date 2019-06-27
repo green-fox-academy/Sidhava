c02f0a6a5e3f80e4cbbade25e853162edb348ff0
Dániel Sümegi <sumegidaniel@gmail.com>
2019-06-27 11:14:38 +0200

    loops covered

---
+++ week-01/day-04/loops.ts
@@ -0,0 +1,10 @@
+let timess: number = 0;
+
+while (timess < 10) {
+    console.log("kihi", timess);
+    timess++;
+}
+
+for (let iterator: number = 0; iterator < 100; iterator++) {
+    console.log(iterator);
+}
