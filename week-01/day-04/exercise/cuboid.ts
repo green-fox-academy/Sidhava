63996d5cf378c2f13babf5d862c34f74749ba4b4
Dániel Sümegi <sumegidaniel@gmail.com>
2019-06-27 15:25:31 +0200

    cuboid.ts exercise

---
+++ week-01/day-04/exercise/cuboid.ts
@@ -0,0 +1,9 @@
+let d1: number = 5;
+let d2: number = 8;
+let d3: number = 10;
+
+let surfaceArea: number = 2 * ((d1 * d2) + (d1 * d3) + (d2 * d3));
+let volume: number = d1 * d2 * d3;
+
+console.log('Surface Area:', surfaceArea);
+console.log('Volume:', volume);
