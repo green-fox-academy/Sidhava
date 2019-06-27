72ac4f0a2cffc184bbeafb84cf786d09c0a22e95
Dániel Sümegi <sumegidaniel@gmail.com>
2019-06-27 16:44:11 +0200

    i-wont-cheat-on-the-exams.ts exercise

---
+++ week-01/day-04/exercise/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,6 @@
+for (let i: number = 0; i < 100; i++) {
+    console.log(i + '. I won\'t cheat on the exam!');
+}
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
