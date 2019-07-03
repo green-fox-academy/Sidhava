4f6365df0d251fcaee60b8539f5cfed13aa78453
Dániel Sümegi <sumegidaniel@gmail.com>
2019-07-03 14:32:14 +0200

    four-rectangles exercise

---
+++ week-02/day-03/exercises/four-rectangles/four-rectangles-ts.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Draw four different size and color rectangles.
+// Avoid code duplication.
+
+let colors: string[] = ['red', 'blue', 'green', 'orange'];
+
+function rectangles(x: number, y: number) {
+    ctx.fillRect(x, y, x, x);
+}
+    for (let i = 0; i <  colors.length; i++) {
+        ctx.fillStyle = colors[i];
+        rectangles(10 * (i + 1), 10 * (i + 1));
+    }
+
+
+
