ab277448a5b20f8b44815bf476fdd15c468af4af
Dániel Sümegi <sumegidaniel@gmail.com>
2019-09-10 15:08:42 +0200

    project so far

--- week-10/project_lvlup/assets/js/app.js
+++ week-10/project_lvlup/assets/js/app.js
@@ -1,31 +1,76 @@
-let addPlaylist = document.querySelector('span');
-let table = document.querySelectorAll('table');
-let tbody = document.querySelectorAll('tbody');
+let addPlaylist = document.querySelector("span");
+let table = document.querySelectorAll("table");
+let tbody = document.querySelectorAll("tbody");
+let tracks = document.querySelector(".tracks");
+let audio = document.querySelector("audio");
+let currentsong = document.querySelector(".current-song");
+let h3 = document.querySelector(".current-title");
+let p = document.querySelector(".current-artist");
 
-addPlaylist.addEventListener('click', () => {
-  if (document.querySelector('input')) {
-    return;
-  } else {
-    let newRow = document.createElement('tr');
-    let rowData = document.createElement('td');
-    /* let addNewPlaylist = document.createElement('form'); */
-    let input = document.createElement('input')
-    input.setAttribute('type', 'text');
+fetch("/playlists")
+  .then(answer => answer.json())
+  .then(result => getPlaylists(result));
+
+fetch("/playlist-tracks")
+  .then(answer => answer.json())
+  .then(result => getTracks(result));
+
+function getPlaylists(result) {
+  result.forEach(element => {
+    let newRow = document.createElement("tr");
+    let rowData = document.createElement("td");
+    rowData.innerHTML = element.playlist;
     tbody[0].appendChild(newRow);
     newRow.appendChild(rowData);
-    /* rowData.appendChild(addNewPlaylist); */
+  });
+}
+
+function getTracks(result) {
+  result.forEach(element => {
+    let newRow = document.createElement("tr");
+    table[1].appendChild(newRow);
+    let rowData = document.createElement("td");
+    let rowData2 = document.createElement("td");
+    let rowData3 = document.createElement("td");
+    rowData.innerHTML = element.id;
+    rowData2.innerHTML = element.title;
+    rowData3.innerHTML = element.duration;
+    newRow.appendChild(rowData);
+    newRow.appendChild(rowData2);
+    newRow.appendChild(rowData3);
+    tracks.addEventListener("click", () => {
+      if (event.target.innerHTML === element.title) {
+        audio.setAttribute("src", element.path);
+        audio.play();
+
+        h3.innerHTML = element.title;
+        p.innerHTML = element.artist;
+      }
+    });
+  });
+}
+
+function playTrack(result) {
+  console.log(result);
+}
+
+addPlaylist.addEventListener("click", () => {
+  if (document.querySelector("input")) {
+    return;
+  } else {
+    let newRow = document.createElement("tr");
+    let rowData = document.createElement("td");
+    let input = document.createElement("input");
+    tbody[0].appendChild(newRow);
+    newRow.appendChild(rowData);
     rowData.appendChild(input);
-    input.addEventListener('keypress', (e) => {
-      if (e.key === 'Enter') {
+    input.focus();
+    input.addEventListener("keypress", e => {
+      if (e.key === "Enter") {
         let inputvalue = input.value;
         rowData.removeChild(input);
         rowData.innerHTML = inputvalue;
       }
-    })
+    });
   }
 });
-
-
-
-
-
--- week-10/project_lvlup/server.js
+++ week-10/project_lvlup/server.js
@@ -4,18 +4,34 @@ const PORT = 3000;
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use(express.static('assets'));
-
-app.get('/', (req, res) => {
-  res.render('index');
-})
+app.set("view engine", "ejs");
+app.use(express.static("assets"));
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'musicplayer'
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "musicplayer"
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/playlists", (req, res) => {
+  connection.query("select playlist from playlists", (err, rows) => {
+    res.send(rows);
+  });
+});
+
+app.get("/playlist-tracks", (req, res) => {
+  connection.query("select * from tracks", (err, rows) => {
+    res.send(rows);
+  });
+});
+
+connection.connect(err => {
+  if (err) throw err;
 });
 
 app.listen(PORT, () => console.log(`app is listening on port: ${PORT}`));
